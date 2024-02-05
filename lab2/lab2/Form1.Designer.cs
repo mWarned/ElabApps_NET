2bbee092e01228998f19d8a6a2e7f1682abf0641
mWarned <107244005+mWarned@users.noreply.github.com>
2024-02-06 01:17:43 +0200

    Add lab2 Windows Forms apps

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.nmProfTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertBtn.Location = new System.Drawing.Point(225, 129);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(92, 28);
+            this.insertBtn.TabIndex = 0;
+            this.insertBtn.Text = "Insereaza ";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // nmProfTxt
+            // 
+            this.nmProfTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nmProfTxt.ForeColor = System.Drawing.Color.Black;
+            this.nmProfTxt.Location = new System.Drawing.Point(56, 70);
+            this.nmProfTxt.Name = "nmProfTxt";
+            this.nmProfTxt.Size = new System.Drawing.Size(189, 26);
+            this.nmProfTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(52, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numele profesorului:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(355, 189);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nmProfTxt);
+            this.Controls.Add(this.insertBtn);
+            this.Name = "Form1";
+            this.Text = "Inserarea datelor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.TextBox nmProfTxt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            string nmProfText = nmProfTxt.Text;
+
+            if (!string.IsNullOrEmpty(nmProfText))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection("Server=YourServerName;Database=YourDatabaseName;Integrated Security=True;"))
+                    {
+                        connection.Open();
+
+                        string query = $"INSERT INTO profesor (idprofesor, nmprofesor) VALUES ('{GetLastIdFromTable(connection) + 1}', '{nmProfText}')";
+
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.ExecuteNonQuery();
+                            MessageBox.Show("Datele au for introduse!");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value in the text field.");
+            }
+        }
+
+        private int GetLastIdFromTable(SqlConnection connection)
+        {
+            string query = "SELECT TOP 1 idprofesor FROM profesor ORDER BY idprofesor DESC";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                var result = command.ExecuteScalar();
+
+                if (result != null && result != DBNull.Value)
+                {
+                    return Convert.ToInt32(result);
+                }
+
+                return 0;
+            }
+        }
+    }
+}
