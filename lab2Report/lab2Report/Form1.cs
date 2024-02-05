ae0c79f427d31c4645741e09b68db19ec378502f
mWarned <107244005+mWarned@users.noreply.github.com>
2024-02-06 01:18:04 +0200

    Add lab2 report app

---
+++ lab2Report/lab2Report/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reportViewer1.LocalReport.ReportPath = "C:\\GitHub\\ElabApps_NET\\lab2Report\\lab2Report\\report.rdl";
+
+            //reportViewer1.LocalReport.DataSources.Add(ds);
+
+            reportViewer1.RefreshReport();
+        }
+    }
+}
