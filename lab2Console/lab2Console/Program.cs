c1d59d5d5a84910614905a76dbac5fa35e02ad86
mWarned <107244005+mWarned@users.noreply.github.com>
2024-02-06 01:16:18 +0200

    Add console app for lab2

---
+++ lab2Console/lab2Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=YourServerName;Database=YourDatabaseName;Integrated Security=True;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM profesor";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["nmprofesor"]}");
+                    }
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
